d47288b1ba325f83397760a51014752ece26c4aa
QuatZo <48104274+QuatZo@users.noreply.github.com>
2019-11-13 12:54:16 +0100

    Changed the loading animation w/ some text style

--- django-react/frontend/src/App.js
+++ django-react/frontend/src/App.js
@@ -7,28 +7,27 @@
     import axios from "axios";
     import FadeIn from "react-fade-in";
     import Lottie from "react-lottie";
-    import ReactLoading from "react-loading";
     import "bootstrap/dist/css/bootstrap.css";
-    import * as legoData from "./legoloading.json";
-    import * as doneData from "./doneloading.json";
+    import * as stillLoadingData from "./stillloading.json";
+    import * as doneLoadingData from "./doneloading.json";
     
-    const defaultOptions = {
+    const defaultOptionsLoading = {
         loop: true,
         autoplay: true,
-        animationData: legoData.default,
+        animationData: stillLoadingData.default,
         rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
+          preserveAspectRatio: "xMidYMid meet"
         }
-        }
+      }
     
-    const defaultOptions2 = {
+    const defaultOptionsLoaded = {
         loop: false,
         autoplay: true,
-        animationData: doneData.default,
+        animationData: doneLoadingData.default,
         rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
+          preserveAspectRatio: "xMidYMid meet"
         }
-        };
+      };
     
     class App extends Component {
       constructor(props) {
@@ -48,29 +47,28 @@
           axios
           .get("http://localhost:8000/api/familytreepersons/")
           .then(res => this.setState({ personList: res.data }))
-            .then(() => {
-               this.setState({ loadingPersonList: true });
-               setTimeout(() => {
-                  this.fetchRelationshipList();
-                  
-               }, 1000);
-            });
+          .then(() => {
+              this.setState({ loadingPersonList: true });
+              setTimeout(() => {
+                this.fetchRelationshipList();
+              }, 1000);
+          });
          }, 1200);
       }
 
       fetchRelationshipList = () => {
-
         this.setState({ donePersonList: true });
+        setTimeout(() => {
         axios
           .get("http://localhost:8000/api/familytreerelationship/")
           .then(res => this.setState({ relationshipList: res.data }))
-            .then(() => {
-              this.setState({ loadingRelationshipList: true });
-              setTimeout(() => {
-                  this.setState({ doneRelationshipList: true });   
-              }, 1000);
-            });
-
+          .then(() => {
+            this.setState({ loadingRelationshipList: true });
+            setTimeout(() => {
+                this.setState({ doneRelationshipList: true });   
+            }, 1000);
+          });
+        }, 1200);
       }
 
       refreshPersonList = () => {
@@ -90,41 +88,37 @@
       render() {
         return (
           <div>
-            <div>
               {!this.state.donePersonList ? (
                 <FadeIn>
-                  <div className="d-flex justify-content-center align-items-center">
-                    <h1 className='error' style={{color: 'white'}}>fetching pizza</h1>
+                  <div className="loading">
+                    <h1 className="display-3">Fetching Persons</h1>
                     {!this.state.loadingPersonList ? (
-                      <Lottie options={defaultOptions} height={120} width={120} />
+                        <Lottie options={defaultOptionsLoading}/>
                     ) : (
-                      <Lottie options={defaultOptions2} height={120} width={120} />
+                      <Lottie options={defaultOptionsLoaded}/>
                     )}
                   </div>
                 </FadeIn>
               ) : !this.state.doneRelationshipList ? (
                 <FadeIn>
-                  <div className="d-flex justify-content-center align-items-center">
-                    <h1 className='error' style={{color: 'white'}}>Jable</h1>
+                  <div className="loading">
+                    <h1 className="display-3">Fetching Relationships</h1>
                     {!this.state.loadingRelationshipList ? (
-                      <Lottie options={defaultOptions} height={120} width={120} />
+                      <Lottie options={defaultOptionsLoading}/>
                     ) : (
-                      <Lottie options={defaultOptions2} height={120} width={120} />
+                      <Lottie options={defaultOptionsLoaded}/>
                     )}
                   </div>
                 </FadeIn>
               ): (
                 <React.Fragment>
-                <Familytree 
-                personList = {this.state.personList}
-                relationshipList = {this.state.relationshipList}
-                />
-                <Todo />
+                  <Familytree 
+                  personList = {this.state.personList}
+                  relationshipList = {this.state.relationshipList}
+                  />
+                  <Todo />
                 </React.Fragment>
-              )}
-            </div>
-            
-            
+              )}            
           </div>
         );
       }
