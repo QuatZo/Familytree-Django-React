5c8cb639a8792667f2f82ede9693560d01e86334
QuatZo <48104274+QuatZo@users.noreply.github.com>
2019-11-23 14:33:29 +0100

    Simple User Authentication w/ Login Page
    * Added JWT Authentication
    * Added login/signup page before loading the content
    * Users authentication exists of only downloading the initial data
    * Above means that you can' add/edit/delete anything, because user doesn't have any permissions (on the React-side)
    * variable ***loginCounter*** is used to not overload the API
    * Added temporary CSS for Login page
    
    * ***Additional note: ***
    Persmissions for add/edit/delete weren't made yet, because API structure needs to be adjusted to the Users Authentication w/ groups & permissions

--- django-react/backend/familytree/views.py
+++ django-react/backend/familytree/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
 
-from rest_framework import viewsets
-from .serializers import FamilytreeSerializer, FamilytreePersonSerializer, FamilytreeRelationshipSerializer
+from rest_framework import viewsets, permissions, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .serializers import FamilytreeSerializer, FamilytreePersonSerializer, FamilytreeRelationshipSerializer, UserSerializer, UserSerializerWithToken
 from .models import Familytree, FamilytreePerson, FamilytreeRelationship
 
 
+@api_view(['GET'])
+def current_user(request):
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+class UserList(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request, format=None):
+        serializer = UserSerializerWithToken(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class FamilytreeView(viewsets.ModelViewSet):
     serializer_class = FamilytreeSerializer
     queryset = Familytree.objects.all()
--- django-react/frontend/src/App.js
+++ django-react/frontend/src/App.js
@@ -1,7 +1,10 @@
 // frontend/src/App.js
 
     import React, { Component } from "react";
-	  import './App.css';
+    import './App.css';
+    import Nav from './components/Nav';
+    import LoginForm from './components/LoginForm';
+    import SignupForm from './components/SignupForm';
     import Familytree from "./Familytree";
     import Todo from "./Todo";
     import axios from "axios";
@@ -38,29 +41,48 @@
           loadingPersonList: undefined,
           donePersonList: undefined,
           loadingRelationshipList: undefined,
-          doneRelationshipList: undefined
+          doneRelationshipList: undefined,
+          displayed_form: '',
+          logged_in: localStorage.getItem('token') ? true : false,
+          username: ''
         };
       }
 
+      loginCounter = 0;
+
       componentDidMount() {
-        setTimeout(() => {
+        if (this.state.logged_in) {
           axios
-          .get("http://localhost:8000/api/familytreepersons/")
-          .then(res => this.setState({ personList: res.data }))
-          .then(() => {
-              this.setState({ loadingPersonList: true });
-              setTimeout(() => {
-                this.fetchRelationshipList();
-              }, 1000);
-          });
-         }, 1200);
+            .get('http://localhost:8000/current_user/', {headers: { Authorization: `JWT ${localStorage.getItem('token')}`}})
+            .then(res => {
+              this.setState({ username: res.username });
+            })
+          }
+      }
+
+      componentDidUpdate(){
+        if (this.loginCounter < 1 && this.state.logged_in && !this.state.doneRelationshipList && !this.state.donePersonList){
+          this.loginCounter += 1;
+          console.log("DID UPDATE!");
+          setTimeout(() => {
+            axios
+              .get("http://localhost:8000/api/familytreepersons/", {headers: { Authorization: `JWT ${localStorage.getItem('token')}`}})
+              .then(res => this.setState({ personList: res.data }))
+              .then(() => {
+                  this.setState({ loadingPersonList: true });
+                  setTimeout(() => {
+                    this.fetchRelationshipList();
+                  }, 1000);
+              });
+          }, 1200);
+        }
       }
 
       fetchRelationshipList = () => {
         this.setState({ donePersonList: true });
         setTimeout(() => {
         axios
-          .get("http://localhost:8000/api/familytreerelationship/")
+          .get("http://localhost:8000/api/familytreerelationship/", {headers: { Authorization: `JWT ${localStorage.getItem('token')}`}})
           .then(res => this.setState({ relationshipList: res.data }))
           .then(() => {
             this.setState({ loadingRelationshipList: true });
@@ -71,54 +93,142 @@
         }, 1200);
       }
 
+      handle_login = (e, data) => {
+        e.preventDefault();
+        const options = {
+          url: 'http://localhost:8000/token-auth/',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: data
+        };
+        axios(options)
+          .then(res => {
+            localStorage.setItem('token', res.data.token);
+            this.setState({
+              logged_in: true,
+              displayed_form: '',
+              username: res.data.username
+            });
+          });
+      };
+    
+      handle_signup = (e, data) => {
+        e.preventDefault();
+        const options = {
+          url: 'http://localhost:8000/users/',
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          data: data
+        };
+        axios(options)
+          .then(res => {
+            localStorage.setItem('token', res.data.token);
+            this.setState({
+              logged_in: true,
+              displayed_form: '',
+              username: res.data.username
+            });
+          });
+      };
+    
+      handle_logout = () => {
+        localStorage.removeItem('token');
+        this.loginCounter = 0;
+        this.setState({ 
+          logged_in: false, 
+          username: '',
+          personList: [],
+          relationshipList: [],
+          relationships: [],
+          loadingPersonList: undefined,
+          donePersonList: undefined,
+          loadingRelationshipList: undefined,
+          doneRelationshipList: undefined,
+        });
+      };
+    
+      display_form = form => {
+        this.setState({
+          displayed_form: form
+        });
+      };
+
       refreshPersonList = () => {
-        axios
+        this.instance
           .get("http://localhost:8000/api/familytreepersons/")
           .then(res => this.setState({ personList: res.data }))
           .catch(err => console.log(err));
       };
 
       refreshRelationshipList = () => {
-        axios
+        this.instance
           .get("http://localhost:8000/api/familytreerelationship/")
           .then(res => this.setState({ relationshipList: res.data }))
           .catch(err => console.log(err));
       };
 
       render() {
+        let form;
+        switch (this.state.displayed_form) {
+          case 'login':
+            form = <LoginForm handle_login={this.handle_login} />;
+            break;
+          case 'signup':
+            form = <SignupForm handle_signup={this.handle_signup} />;
+            break;
+          default:
+            form = null;
+        }
         return (
-          <div>
-              {!this.state.donePersonList ? (
-                <FadeIn>
-                  <div className="loading">
-                    <h1 className="display-3">Fetching Persons</h1>
-                    {!this.state.loadingPersonList ? (
-                        <Lottie options={defaultOptionsLoading}/>
-                    ) : (
-                      <Lottie options={defaultOptionsLoaded}/>
-                    )}
-                  </div>
-                </FadeIn>
+          <div className="App">
+            <Nav
+              logged_in={this.state.logged_in}
+              display_form={this.display_form}
+              handle_logout={this.handle_logout}
+            />
+            {form}
+            {this.state.logged_in
+              ? !this.state.donePersonList ? (
+                <h3>
+                  <FadeIn>
+                    <div className="loading">
+                      <h1 className="display-3">Fetching Persons</h1>
+                      {!this.state.loadingPersonList ? (
+                          <Lottie options={defaultOptionsLoading}/>
+                      ) : (
+                        <Lottie options={defaultOptionsLoaded}/>
+                      )}
+                    </div>
+                  </FadeIn>
+                </h3>
               ) : !this.state.doneRelationshipList ? (
-                <FadeIn>
-                  <div className="loading">
-                    <h1 className="display-3">Fetching Relationships</h1>
-                    {!this.state.loadingRelationshipList ? (
-                      <Lottie options={defaultOptionsLoading}/>
-                    ) : (
-                      <Lottie options={defaultOptionsLoaded}/>
-                    )}
-                  </div>
-                </FadeIn>
+                <h3>
+                  <FadeIn>
+                    <div className="loading">
+                      <h1 className="display-3">Fetching Relationships</h1>
+                      {!this.state.loadingRelationshipList ? (
+                        <Lottie options={defaultOptionsLoading}/>
+                      ) : (
+                        <Lottie options={defaultOptionsLoaded}/>
+                      )}
+                    </div>
+                  </FadeIn>
+                </h3>
               ): (
                 <React.Fragment>
                   <Familytree 
                   personList = {this.state.personList}
                   relationshipList = {this.state.relationshipList}
                   />
-                  <Todo />
+                  {//<Todo />
+                  }
                 </React.Fragment>
-              )}            
+              )      
+              : 'Please, log in.'}
           </div>
         );
       }
