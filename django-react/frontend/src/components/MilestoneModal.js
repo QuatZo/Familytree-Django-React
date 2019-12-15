59b9cf96ca97fc703d023a654622f4a187dc7845
QuatZo <48104274+QuatZo@users.noreply.github.com>
2019-12-15 17:07:14 +0100

    Multi-Select for Add Milestone
    * Added multi select dropdowm list for Add Milestone menu
    * Added validation for persons in Add Milestone (at least one)
    * Added automatic update for person list in Add Milestone

--- django-react/frontend/src/components/MilestoneModal.js
+++ django-react/frontend/src/components/MilestoneModal.js
@@ -2,8 +2,12 @@
 /*eslint no-useless-computed-key: 0*/
 
     import React, { Component } from "react";
+    import axios from "axios";
     import DatePicker from "react-datepicker";
     import 'react-datepicker/dist/react-datepicker.css';
+    import MultiSelect from "@khanacademy/react-multi-select";    
+    import NOTIFY from '../Enums.ts';
+    import ShowNotification from './Notification';
     
     import {
       Button,
@@ -33,7 +37,29 @@
             title: false,
             date: false,
           },
+          personSelectOptions: [],
         };
+      }
+
+      componentDidMount(){
+        var options = [];
+        axios
+          .get("http://localhost:8000/api/familytreepersons/", {
+            headers: { Authorization: `JWT ${localStorage.getItem('token')}`}
+          })
+          .then(res => {
+            res.data.map(item => {
+              options.push({
+                label: item.first_name + " " + item.last_name,
+                value: item.id,
+              });
+            });
+            this.setState({personSelectOptions: options});
+          })
+          .catch(err => {
+            console.log(err);
+            ShowNotification(NOTIFY.ERROR);
+          });
       }
 
       handleChange = (e) => {
@@ -47,10 +73,11 @@
         this.setState({activeItem});
       };
 
-      validate(title, date){
+      validate(title, date, person_id){
         return{
           title: title.trim().length === 0,
-          date: date.toString().trim().length === 0
+          date: date.toString().trim().length === 0,
+          person_id: person_id.length === 0
         }
       }
 
@@ -62,7 +89,7 @@
       
       render() {
         const { toggle, onSave } = this.props;
-        const errors = this.validate(this.state.activeItem.title, this.state.activeItem.date);
+        const errors = this.validate(this.state.activeItem.title, this.state.activeItem.date, this.state.activeItem.person_id);
         const isEnabled = !Object.keys(errors).some(x => errors[x]);
         return (
           <Modal isOpen={true} toggle={toggle}>
@@ -105,9 +132,24 @@
                     showYearDropdown
                     dropdownMode="select"/>
                 </FormGroup>
+                <FormGroup>
+                  <Label for="person_id">Persons {errors.person_id ? " (please, choose at least one person from the list)" : null}</Label><br />
+                  <MultiSelect
+                    options={this.state.personSelectOptions}
+                    className={"form-control " + (errors.person_id ? "error" : "")}
+                    selected={this.state.activeItem.person_id}
+                    onSelectedChanged={selected => this.setState({ activeItem: {
+                        user_id: this.state.activeItem.user_id,
+                        person_id: selected,
+                        date: this.state.activeItem.date,
+                        title: this.state.activeItem.title,
+                        text: this.state.activeItem.text,
+                        image: this.state.activeItem.image,
+                      }})}
+                  />
+                  </FormGroup>
                 {/* Missing: 
                   - File upload
-                  - Associated persons picker (multi-select)
                 */}
               </Form>
             </ModalBody>
