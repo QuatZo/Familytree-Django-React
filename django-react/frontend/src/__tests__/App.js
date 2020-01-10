d12d7d9af9d000e020b77ee0e965a1062144ab63
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-01-10 21:13:22 +0100

    React Test - Part 2
    * Added few tests for App.js
    * Added similar tests for Login Form, as for App.js
    * Added similar tests for Signup Form, as for App.js
    * Added fields & buttons tests for Login Form
    * Added fields & buttons tests for Signup Form
    * Added disabled/enabled tests for Signup Form (included sufficient & insufficient password)

--- django-react/frontend/src/__tests__/App.js
+++ django-react/frontend/src/__tests__/App.js
@@ -4,22 +4,47 @@ import App from '../App';
 import LoginForm from '../components/auth/LoginForm';
 import Nav from '../components/nav/Nav';
 import Familytree from '../Familytree';
+import SignupForm from '../components/auth/SignupForm';
 
 it('renders without crashing', () => {
   shallow(<App />);
 });
 
-it('includes nav bar', () => {
+it('includes nav bar at page load', () => {
   const app = shallow(<App />);
   expect(app.containsMatchingElement(<Nav />)).toEqual(true)
 });
 
-it('includes Login Form', () => {
+it('includes Login Form at page load', () => {
   const app = shallow(<App />);
   expect(app.containsMatchingElement(<LoginForm />)).toEqual(true)
 });
 
-it('doesn\'t include Familytree', () => {
+it('doesn\'t include Signup Form at page load', () => {
+  const app = shallow(<App />);
+  expect(app.containsMatchingElement(<SignupForm />)).not.toEqual(true)
+});
+
+it('doesn\'t include Familytree at page load', () => {
   const app = shallow(<App />);
   expect(app.containsMatchingElement(<Familytree />)).not.toEqual(true)
+});
+
+it('includes Familytree after log in', () => {
+  const app = shallow(<App />);
+  app.setState({
+    displayed_form: '',
+    donePersonList: true,
+    doneRelationshipList: true,
+    logged_in: true,
+  })
+  expect(app.containsMatchingElement(<Familytree />)).toEqual(true)
+});
+
+it('includes Signup Form after click on \'Register\'', () => {
+  const app = shallow(<App />);
+  app.setState({
+    displayed_form: 'signup',
+  })
+  expect(app.containsMatchingElement(<SignupForm />)).toEqual(true)
 });
