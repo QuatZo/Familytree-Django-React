5432fe0d418f72e551706ab72d046616c776272c
QuatZo <48104274+QuatZo@users.noreply.github.com>
2020-01-25 16:19:53 +0100

    Build Update

---
+++ django-react/frontend/build/precache-manifest.4194d64d1853961d2ec8598c073646cb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529fe8e2b77e20b72ea0fe30edc589d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "462acdb734947adff7fe",
+    "url": "/static/css/2.e26dfd9f.chunk.css"
+  },
+  {
+    "revision": "edba6070eda5b213cd97",
+    "url": "/static/css/main.ef9de6ff.chunk.css"
+  },
+  {
+    "revision": "462acdb734947adff7fe",
+    "url": "/static/js/2.397146c2.chunk.js"
+  },
+  {
+    "revision": "dac4e015edee8727e6953e951df656c8",
+    "url": "/static/js/2.397146c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edba6070eda5b213cd97",
+    "url": "/static/js/main.5420f892.chunk.js"
+  },
+  {
+    "revision": "810b7eddbba2845a5756",
+    "url": "/static/js/runtime-main.4e64986e.js"
+  }
+]);
